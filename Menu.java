9791b3bab0818f5595b3b60641ad48e4d3ff02b1
existinghuman99 <thehumanej@gmail.com>
2023-11-15 19:04:17 -0600

    changes on my desktop that i forgot to commit erlr

--- Menu.java
+++ Menu.java
@@ -3,14 +3,34 @@ import java.util.Scanner;
 public class Menu {
     static void runMenu() {
         // basic stuff & selection options
+
         System.out.println("🅜 🅔 🅣 🅡 🅘 🅒 🅞 🅝 🅥 🅔 🅡 🅣 🅔 🅡");
         System.out.println("");
         System.out.println("Conversion options:");
         System.out.println("1. Feet to Meters");
         System.out.println("2. Inches to Centimeters");
         System.out.println("3. Miles to Kilometers");
+        System.out.println("");
+        System.out.print("Your selection: ");
         
         // scanner stuff
         
+        int menuSelection;
+        Scanner ms = new Scanner(System.in);
+        menuSelection = ms.nextInt();
+        
+        // selection
+
+        double conversionInput;
+        Scanner cs = new Scanner(System.in);
+        conversionInput = cs.nextInt();
+
+        if (menuSelection == 1) {
+            System.out.println("");
+            System.out.println("Enter value in feet to be converted to meters: ");
+            conversionInput = cs.nextInt();
+            //metricToImp.feetToMeters(conversionInput);
+            System.out.println(metricToImp.feetToMeters(conversionInput));
+        }
     }
 }
